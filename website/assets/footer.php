8c8ca930d5e49bb46217ff5e12e61ee1549d5967
Lennart Lösche <loesche.lennart@gmail.com>
2021-04-19 06:49:57 +0200

    Delete website directory

--- website/assets/footer.php
+++
@@ -1,8 +0,0 @@
-<hr>
-<p class="container padding">
-    karmatop.de ist eine Community Website und steht nicht in Verbindung mit der tixia UG (haftungsbeschränkt) oder dem Timolia Netzwerk.<br>
-    Wir bemühen uns die Liste so aktuell wie möglich zu halten, trotzdem besteht keine Garantie auf Aktualität und Richtigkeit der Daten!
-<p>
-<p>
-    <a href="https://lennartloesche.de/datenschutz.html">Datenschutz</a> - <a href="https://lennartloesche.de/impressum.html">Impressum</a>
-<p>
