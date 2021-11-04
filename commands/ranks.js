5df3121739c57680943159d5966c147bbee6f179
l3nnartt <loesche.lennart@gmail.com>
2021-11-04 15:41:55 +0100

    Add new ranks command

--- commands/ranks.js
+++ commands/ranks.js
@@ -1,13 +1,13 @@
-const Discord = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton, MessageEmbed } = require('discord.js');
 
 module.exports = {
-	name: 'ranks',
-    aliases: ['roles', 'rollen'],
-	description: 'Übersicht aller Ränge auf dem Discord',
-	cooldown: 5,
-	execute(message, args, client) {
-		const embed1 = new Discord.MessageEmbed()
-			.setTitle(`${client.user.username} • 4rena Ränge`)
+    data: new SlashCommandBuilder()
+        .setName('ranks')
+        .setDescription('Übersicht aller 4rena-Ränge auf dem Discord'),
+    async execute(interaction, client) {
+        const embed = new MessageEmbed()
+            .setTitle(`${client.user.username} • 4rena Ränge`)
             .addFields(
                 { name: 'Camper', value: `64 Punkte <@&842058723862380575>`},
                 { name: 'Faustkämpfer', value: `128 Punkte <@&617079299354263562>`},
@@ -20,9 +20,9 @@ module.exports = {
                 { name: 'Profikiller', value: `16348 Punkte <@&842057955843244032>`},
                 { name: 'Meisterkiller', value: `32768 Punkte <@&617079551843106816>`},
                 { name: 'Killaura', value: `65536 Punkte <@&848506456969445377>`})
-			.setTimestamp(message.createdAt)
-			.setFooter(`${client.user.username}`, client.user.displayAvatarURL())
-			.setColor("#4680FC");
-		message.channel.send(embed1)
-	}
+            .setTimestamp(interaction.createdAt)
+            .setFooter(`${client.user.username}`, client.user.displayAvatarURL())
+            .setColor("#4680FC");
+        interaction.reply({embeds: [embed]});
+    },
 };
