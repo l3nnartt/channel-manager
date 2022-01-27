f98725fbb56de0d8dbb65af05f72dbc216323301
l3nnartt <loesche.lennart@gmail.com>
2022-01-27 02:35:07 +0100

    Update depends + remove message log

--- deploy-commands.js
+++ deploy-commands.js
@@ -26,7 +26,7 @@ const rest = new REST({ version: '9' }).setToken(token);
             //Routes.applicationCommands(clientId),
             { body: commands },
         );
-        console.log(commands)
+        //console.log(commands)
         console.log('Successfully reloaded application (/) commands.');
     } catch (error) {
         console.error(error);
