5e86032ce3a9ff178b669a2e9e1abaf9df68e01c
l3nnartt <loesche.lennart@gmail.com>
2022-01-27 02:05:14 +0100

    fix voice channel ENDLICH

---
+++ events/voiceStateUpdate.js
@@ -0,0 +1,37 @@
+module.exports = {
+    name: 'voiceStateUpdate',
+    async execute(oldState, newState, client) {
+        const user = await client.users.fetch(newState.id);
+
+        if (newState.channel === null) return;
+        if (newState.channel.parent.id === '833501385454518272') {
+
+            const channelName = newState.channel.name;
+            const joined = !!newState.channelId
+            const channelId = joined ? newState.channelId : oldState.channelId
+            let channel = newState.guild.channels.cache.get(channelId)
+
+            const {
+                type,
+                userLimit,
+                bitrate,
+                edit,
+                rawPosition,
+            } = channel
+
+            newState.guild.channels.create(channelName, {
+                type,
+                bitrate,
+                userLimit,
+                parent: '833689325006094346',
+                edit,
+                position: rawPosition,
+            }).then((channel) => {
+                newState.member.voice.setChannel(channel)
+            })
+        }
+
+        if (oldState.channel === null) return;
+        if (oldState.channel.members.size === 0 && oldState.channel.parent.id === '833689325006094346') return oldState.channel.delete();
+    },
+};
