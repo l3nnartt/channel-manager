bbc652fa144e487d3a491bbec8ea80aca492beab
l3nnartt <loesche.lennart@gmail.com>
2022-01-27 02:26:41 +0100

    update message log

--- events/message.js
+++ events/message.js
@@ -4,24 +4,23 @@ const Discord = require("discord.js");
 module.exports = {
 	name: 'messageCreate',
 	execute(message, client) {
-		if (message.channel.type === 'dm') {
+		if (message.type === 'DEFAULT' && message.author.id !== client.user.id) {
 			
 			//TimeStamp
 			const timestamp = new Date()
 
 			//File Log
-			fs.appendFile(`./debug.log`, `timestamp: ${timestamp};\t Author: ${message.author.tag};\t Content: ${message.content};\n`, function (err) {
+			fs.appendFile(`./debug.log`, `timestamp: ${timestamp};\t Author: ${message.author.username}#${message.author.discriminator};\t Content: ${message.content};\n`, function (err) {
 				if (err) throw err;
 			});
 
 			//Discord Log
 			const embed = new Discord.MessageEmbed()
-				.setAuthor({ name: `${message.author.tag}`, iconURL: message.author.displayAvatarURL() })
-				.setDescription(`${message.content}`)
+				.setAuthor({ name: `${message.author.username} ${message.author.discriminator}`, iconURL: message.author.displayAvatarURL() })
+				.setDescription(`${message.content} \n\n Channel: <#${message.channelId}>`)
 				.setTimestamp(message.createdAt)
-				.setFooter({ text: `${client.user.username} Log-System` })
 				.setColor("#2a2a2a");
-			client.channels.fetch('833713378194620416').then(channel => channel.send(embed));
+			client.channels.fetch('727167785599238155').then(channel => channel.send({embeds: [embed]}));
 		}
 	},
 };
